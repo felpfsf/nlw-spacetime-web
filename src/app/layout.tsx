b33d91912d66380c3ba0831dcb7567ef8b396f0e
Felipe <feferreira.sf@gmail.com>
2023-05-16 13:22:25 -0300

    feat: initial

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,18 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
+import './globals.css'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "NLW Spacetime",
-  description: "Compartilhe seus momentos",
-};
+  title: 'NLW Spacetime',
+  description: 'Compartilhe seus momentos',
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang="pt">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello Next</h1>
+      <h1 className="text-4xl font-extrabold">Hello Next</h1>
     </main>
   );
 }
