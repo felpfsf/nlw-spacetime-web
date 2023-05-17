9be177d2a4317c19e40efe76b62ce6fc069dd99c
Felipe <feferreira.sf@gmail.com>
2023-05-17 13:23:59 -0300

    feat: fonts import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
-import { Inter } from 'next/font/google'
-import { ReactNode } from 'react'
-import './globals.css'
+import {
+  Bai_Jamjuree as BaiJamjuree,
+  Roboto_Flex as Roboto,
+} from "next/font/google";
+import { ReactNode } from "react";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+const baijamjuree = BaiJamjuree({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-baijamjuree",
+});
 
 export const metadata = {
-  title: 'NLW Spacetime',
-  description: 'Compartilhe seus momentos',
-}
+  title: "NLW Spacetime",
+  description: "Compartilhe seus momentos",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} ${baijamjuree.variable} font-sans`}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
