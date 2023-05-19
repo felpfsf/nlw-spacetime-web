2caa6fd8bb9d5587cbb7d86166ea5d312c2a2308
Felipe <feferreira.sf@gmail.com>
2023-05-19 16:40:26 -0300

    feat: fetch memories from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
 import EmptyMemories from "@/components/EmptyMemories";
+import { api } from "@/lib/api";
+import { cookies } from "next/headers";
 
-export default function Home() {
-  return <EmptyMemories />;
+export default async function Home() {
+  const isAuthenticated = cookies().has("spacetimeToken");
+  if (!isAuthenticated) {
+    return <EmptyMemories />;
+  }
+
+  const token = cookies().get("spacetimeToken")?.value;
+
+  const response = await api.get("/api/memories/", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const memories = response.data;
+  console.log(response.data)
+  console.log(token)
+
+  return <div>{JSON.stringify(memories)}</div>;
 }
